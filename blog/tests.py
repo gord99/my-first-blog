04e986ecfc962dcfddd938bcfc8c55f57d74f585
gord99 <jrong@afsenyc.org>
2016-05-13 16:16:06 -0400

    We fixed the bug

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,5 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
